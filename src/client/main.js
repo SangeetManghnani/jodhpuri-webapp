3d6c3c3372bab43fcb12e4852231f0df9e470a0a
Sangeet Manghnani <smanghna@in.ibm.com>
2018-01-05 14:06:55 +0530

    init commit- material ui css added

--- src/client/main.js
+++ src/client/main.js
@@ -1,4 +1,25 @@
 require('./_main.scss');
+import $ from "jquery";
+let vm = new Vue({
+    el: '#app',
+    data() {
+        return {}
+    },
+    methods: {}
+})
 
 console.log("qwel");
 
+export class main {
+    constructor() {
+        console.log('hello')
+        $('#Home-nav-link').click((e) => {
+            e.preventDefault();
+            alert('Jquery works!')
+        });
+    }
+}
+
+const mainVar = new main;
+
+// export main;
--- src/server/app/home/home-controller.js
+++ src/server/app/home/home-controller.js
@@ -1,9 +1,19 @@
 const controller = {};
 
 controller.main = function(req, res) {
-  res.render('home/home', {
-
-  });
+    res.render('home/home', {
+        pageTitle: 'Hello Sangeet',
+        navList: [{
+            name: "Home",
+            link: '/home'
+        }, {
+            name: "Login",
+            link: '/login'
+        }, {
+            name: "Signup",
+            link: '/signup'
+        }]
+    });
 };
 
 export default controller;
